14c8813770f986b1ca43d19d9030a004e7d8a1d6
AsanHO <gorma00@naver.com>
2022-07-11 20:32:06 +0900

    grid remake

--- js/color.js
+++ js/color.js
@@ -2,18 +2,11 @@ const randomColor = [
   {
     color: "linear-gradient(-45deg,#ffa69e,#e58c8a,#eec0c6)",
   },
-  {
-    color: "linear-gradient(-45deg,#9fa4c4,#b3cdd1,#bdd4e7,#8693ab)",
-  },
+
   {
     color: "linear-gradient(-45deg,#a7acd9,#9e8fb2,#90d5ec, #fc575e)",
   },
-  {
-    color: "linear-gradient(-45deg,#ffd8cb,#f9d29d,#fad0c4,#f1a7f1)",
-  },
-  {
-    color: "linear-gradient(-45deg,#bbf0f3,#f6d285,#e8dbfc,#f8f9d2)",
-  },
+
   {
     color: "linear-gradient(-45deg,#09203f,#537895,#2f4353,#d2ccc4)",
   },
